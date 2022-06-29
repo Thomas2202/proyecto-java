fc8533285ea2d6cffaf438335b35fe2a08d14823
Thomas <104038682+Thomas2202@users.noreply.github.com>
2022-06-28 23:41:48 -0300

    Retoques

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-const edadPersona = 20;
+const edadPersona = 18;
 const dineroEnBolsillo = 1000;
+const dineroDeLaEntrada = 1000;
+const nombrePersona = 'jorge'
 
-if(edadPersona >= 18 && dineroEnBolsillo >= 1000){
+if(((edadPersona >= 18) || dineroEnBolsillo >= dineroDeLaEntrada) || nombrePersona == 'Thomas'){
     console.log("Bienvenido a nuestro bar")
     if (edadPersona != 18){
         console.log("Felicitaciones, sos mayor pero no tenes 18")
