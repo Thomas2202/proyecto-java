59bd9491d89f26e086d7fc6b7396204e097a5db8
Thomas <104038682+Thomas2202@users.noreply.github.com>
2022-06-29 02:10:53 -0300

    Desafio js condicional

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-const edadPersona = 18;
-const dineroEnBolsillo = 1000;
-const dineroDeLaEntrada = 1000;
-const nombrePersona = 'jorge'
+const edadPersona = prompt('Ingrese su edad:' )
+const dineroEnBilletera = prompt('Ingrese su dinero: ')
+const valorEntrada = 2000
+const nombrePersona = prompt('Ingrese su nombre: ') 
+let pase = 'Bienvenido. Podes pasar.'
+let noPasa = 'Lo sentimos. No podes pasar.'
 
-if(((edadPersona >= 18) || dineroEnBolsillo >= dineroDeLaEntrada) || nombrePersona == 'Thomas'){
-    console.log("Bienvenido a nuestro bar")
-    if (edadPersona != 18){
-        console.log("Felicitaciones, sos mayor pero no tenes 18")
-    }
-} else{
-    console.log("No podes entrar, sos menor :(")
+if(((edadPersona >= 18) && dineroEnBilletera >= valorEntrada) || nombrePersona == 'Thomas'){
+  console.log('Podes pasar')
+  alert(pase + ' ' + nombrePersona)
+}
+else{
+  console.log('No podes pasar')
+  alert(noPasa + ' ' + nombrePersona)
 }
