6161091e6e7c476f1185a92ab7bd160bfe082fb7
Thomas <104038682+Thomas2202@users.noreply.github.com>
2022-07-12 19:37:48 -0300

    Simulador interactivo

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-const numb = parseInt(prompt('Ingresar la cantidad de veces que quiera repetir la palabra'))
-
-const txt = '¡Hola!'
-
-for(let i = 0; i < numb; i++) {
-    alert(txt);
+const COTIZACION_DOLAR = 127;
+const cotizarDolar = (pesos) => pesos / COTIZACION_DOLAR;
+const cotizarPesos = (dolar) => dolar * COTIZACION_DOLAR;
+let seleccion = prompt("Seleccionar cotizacion \n 1 - Dolares a pesos argentinos \n 2 -  Pesos argentinos a dolar ");
+let valor = prompt("VALOR");
+switch (seleccion) {
+    case "1":
+        alert(cotizarPesos(valor));
+        break;
+    case "2":
+        alert(cotizarDolar(valor));
+        break;
+    default:
+        break;
 }
