aa72277480d81ca498cd7c260675a6905147e5c7
Thomas <104038682+Thomas2202@users.noreply.github.com>
2022-07-12 23:01:46 -0300

    Desafio Funciones

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
-const COTIZACION_DOLAR = 127;
-const cotizarDolar = (pesos) => pesos / COTIZACION_DOLAR;
-const cotizarPesos = (dolar) => dolar * COTIZACION_DOLAR;
-let seleccion = prompt("Seleccionar cotizacion \n 1 - Dolares a pesos argentinos \n 2 -  Pesos argentinos a dolar ");
-let valor = prompt("VALOR");
-switch (seleccion) {
-    case "1":
-        alert(cotizarPesos(valor));
-        break;
-    case "2":
-        alert(cotizarDolar(valor));
-        break;
-    default:
-        break;
+let nombre = prompt("Ingresa tu nombre: ")
+
+let saludar = (nombre) => {alert("Bienvenido " + nombre + ", veremos si promocionas.")}
+
+saludar(nombre)
+
+
+let nota1 = parseInt(prompt("Ingresa la nota del primer parcial"))
+
+while(nota1 > 11 || nota1 < 1){
+    alert("La nota debe ser mayor a 0 y menor o igual a 10")
+    nota1 = parseInt(prompt("Ingresa la nota del primer parcial"))
+} 
+
+
+
+let nota2 = parseInt(prompt("Ingresa la nota del segundo parcial"))
+
+while(nota2 > 11 || nota2 < 1){
+    alert("La nota debe ser mayor a 0 y menor o igual a 10")
+    nota2 = parseInt(prompt("Ingresa la nota del segundo parcial"))
+} 
+
+let promedio = (n1,n2) => {
+    let resultado = (n1+n2)/2
+    return  resultado
 }
+
+
+let promedioNotas = promedio(nota1, nota2)
+
+if(promedioNotas>=7){
+    alert("Felicidades " + nombre + "! Promocionaste con un promedio de: " + promedioNotas)
+}else{ alert("Lo siento " + nombre + ", tu promedio es de " + promedioNotas + " y no promocionaste.") }
